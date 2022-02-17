dbd9ce22ae64d468d482bc32c4d4d7ddf41b108e
Daochun Zhao <daochun.zhao@iherb.com>
2022-02-17 08:32:29 +0800

    update

--- golang/main.go
+++ golang/main.go
@@ -1,61 +1,15 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
+
+const (
+	i = 7
+	j
+	k
 )
 
-type Phone interface {
-	Call()
-	SendMsg(s string) bool
-}
-
-type Android struct {
-	Name string
-}
-
-func (a Android) Call() {
-	fmt.Println("call")
-}
-func (a Android) SendMsg(s string) bool {
-	fmt.Println(s)
-	return true
-}
-
-type Ios struct{}
-
-func (a Ios) Call() {
-	fmt.Println("call")
-}
-func (a Ios) SendMsg(s string) bool {
-	fmt.Println(s)
-	return true
-}
-
 func main() {
-	str := "hello"
-	fmt.Println(len(str))
-	for _, v := range str {
-		fmt.Println(string(v))
-	}
-
-	/*
-		k := []int{1, 2, 3, 4, 5}
-		i := 4
-
-		copy(k[i:], k[i+1:])
-		k = k[:len(k)-1]
-		fmt.Println(k)
-
-		x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-		for idx := 0; idx < len(x); idx++ {
-			if x[idx]%3 == 0 {
-				copy(x[idx:], x[idx+1:])
-				x = x[:len(x)-1]
-			}
-			if idx+1 > len(x) {
-				break
-			}
-		}
-		fmt.Println(x)
-	*/
+	a := []string{"a", "b", "c"}
+	a = nil
+	fmt.Println(a, len(a), cap(a))
 }
