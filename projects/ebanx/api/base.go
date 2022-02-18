17ce20d8055382c9535df0a1ea507c96e7805a87
Daochun Zhao <daochun.zhao@iherb.com>
2022-02-18 14:30:07 +0800

    remove some useless codes

--- projects/ebanx/api/base.go
+++ projects/ebanx/api/base.go
@@ -1,28 +1,5 @@
 package api
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 type Base struct {
 	//
-}
-
-func responseFail(ctx *gin.Context, err error) {
-	ctx.AbortWithStatusJSON(http.StatusOK, "")
-}
-
-type jsonResponse struct {
-	Status string        `json:"status,omitempty"` // success | fail
-	Data   interface{}   `json:"data,omitempty"`
-	Error  responseError `json:"error,omitempty"`
-}
-type responseError struct {
-	Message string `json:"message,omitempty"`
-}
-
-type PortalCtx struct {
-	*gin.Context
 }
